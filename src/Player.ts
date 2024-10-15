74fcf6ca55eff39b62e6f646865c2253269a15e1
Artyom Koval <artyom.koval@vimcar.com>
2024-10-15 15:22:29 +0200

    timeout check for api call

--- src/Player.ts
+++ src/Player.ts
@@ -22,20 +22,24 @@ export class Player {
       const knownCards = [...communityCards, ...holeCards];
       if (knownCards.length >= 5) {
         type Hand = Array<{ rank: string; suit: string }>;
+        const timeoutPromise = new Promise((resolve) => {
+            setTimeout(resolve, 2000)
+        })
         const checkRanks = async (cards: Hand) => {
-          const query = new URLSearchParams();
-          query.set("cards", JSON.stringify(cards));
+            const query = new URLSearchParams();
+            query.set("cards", JSON.stringify(cards));
 
-          const response = await fetch(
-            `https://rainman.leanpoker.org/?${query.toString()}`
-          );
-          if (response.ok) {
-            const data = await response.json();
-            const rank = data.rank;
-            betCallback(rank * 100);
-          }
+            const response = await fetch(
+                `https://rainman.leanpoker.org/?${query.toString()}`
+            );
+            if (response.ok) {
+                const data = await response.json();
+                const rank = data.rank;
+                betCallback(rank * 100);
+            }
         };
-        await checkRanks(knownCards);
+        Promise.race([checkRanks, timeoutPromise])
+        betCallback(50);
       }
     } else {
       // pre-flop
