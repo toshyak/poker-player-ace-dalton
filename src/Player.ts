83c13ea134739ec6cf8d49de6b32f8192a61485f
Alexey Osheychik <alexey.osheychik@vimcar.com>
2024-10-15 14:13:03 +0200

    Bet more if have pair

--- src/Player.ts
+++ src/Player.ts
@@ -1,11 +1,23 @@
 export class Player {
-  public betRequest(gameState: any, betCallback: (bet: number) => void): void {
-    betCallback(5);
-  }
+    public betRequest(
+        gameState: any,
+        betCallback: (bet: number) => void
+    ): void {
+        //iterate over players
+        //check if we have a pair
+        for (let player of gameState.players) {
+            if (player.hole_cards) {
+                if (player.hole_cards[0].rank === player.hole_cards[1].rank) {
+                    betCallback(50);
+                    return;
+                }
+            }
 
-  public showdown(gameState: any): void {
+            betCallback(5);
+        }
+    }
 
-  }
-};
+    public showdown(gameState: any): void {}
+}
 
 export default Player;
