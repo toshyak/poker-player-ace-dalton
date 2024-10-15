ca1edda9a2f369bf30c25be838c28b9ab26cfc3f
Alexey Osheychik <alexey.osheychik@vimcar.com>
2024-10-15 14:08:51 +0200

    Always bet 5

--- src/Player.ts
+++ src/Player.ts
@@ -1,6 +1,6 @@
 export class Player {
   public betRequest(gameState: any, betCallback: (bet: number) => void): void {
-    betCallback(0);
+    betCallback(5);
   }
 
   public showdown(gameState: any): void {
